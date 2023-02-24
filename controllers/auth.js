26f2c22aaf20a6c2f2101ebb76c699c2bf187b5f
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-02-24 13:40:54 +0200

    use avatars

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,73 +1,104 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
 
-const {User} = require("../models/user");
+const { User } = require("../models/user");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
-const register = async(req, res)=> {
-    const {email, subscription, password} = req.body;
-    const user = await User.findOne({email});
-    if(user) {
-        throw HttpError(409, `${email} is already exist`)
-    }
-    
-    const hashPassword = await bcrypt.hash(password, 10);
+const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
-    const result = await User.create({subscription, email, password: hashPassword});
+const register = async (req, res) => {
+  const { email, subscription, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, `${email} is already exist`);
+  }
 
-    res.json({
-        email: result.email,
-        subscription: result.subscription,
-    })
-}
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-const login = async(req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if(!user) {
-        throw HttpError(401, "Email or password invalid"); // throw HttpError(401, "Email invalid");
-    }
+  const result = await User.create({
+    subscription,
+    email,
+    password: hashPassword,
+    avatarURL,
+  });
 
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if(!passwordCompare) {
-        throw HttpError(401, "Email or password invalid"); // throw HttpError(401, "Password invalid");
-    }
+  res.json({
+    email: result.email,
+    subscription: result.subscription,
+  });
+};
 
-    const payload = {
-        id: user._id,
-    }
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid"); // throw HttpError(401, "Email invalid");
+  }
 
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
-    await User.findByIdAndUpdate(user._id, {token});
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid"); // throw HttpError(401, "Password invalid");
+  }
 
-    res.json({
-        token,
-    })
-}
+  const payload = {
+    id: user._id,
+  };
 
-const getCurrent = async(req, res) => {
-    const {subscription, email} = req.user;
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
 
-    res.json({
-        subscription,
-        email,
-    })
-}
+  res.json({
+    token,
+  });
+};
 
-const logout = async(req, res) => {
-    await User.findByIdAndUpdate(req.user._id, {token: ""});
+const updateAvatar = async (req, res) => {
+  if (!req.file) {
+    throw HttpError("400", "avatar must be exist");
+  }
 
-    res.json({
-        message: "Logout success"
-    })
-}
+  const { path: tempUpload, originalname } = req.file;
+  const { _id } = req.user;
+  const extenstion = originalname.split(".").pop();
+  const filename = `${_id}_avatar.${extenstion}`;
+  const resultUpload = path.join(avatarDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
+const getCurrent = async (req, res) => {
+  const { subscription, email } = req.user;
+
+  res.json({
+    subscription,
+    email,
+  });
+};
+
+const logout = async (req, res) => {
+  await User.findByIdAndUpdate(req.user._id, { token: "" });
+
+  res.json({
+    message: "Logout success",
+  });
+};
 
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-    getCurrent: ctrlWrapper(getCurrent),
-    logout: ctrlWrapper(logout),
-}
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
+};
